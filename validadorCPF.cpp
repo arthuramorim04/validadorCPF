1a3073682c209fcfba2e2d666a71c6d4d9851067
Arthur Amorim <arthurpas67@gmail.com>
2019-08-26 13:36:17 -0300

    Update and rename validador.cpp to validadorCPF.cpp

--- validadorCPF.cpp
+++ validadorCPF.cpp
@@ -6,15 +6,15 @@ using namespace std;
 
 void validadorCPF();
 
-long long cpfNumeros[11], digito1, digito2;
+long long int cpfNumeros[11], digito1, digito2;
 char cpf[11];
 int i;
 
 void receberCpf()
 {
 
+    //recebe CPF como char, converte pra int e salva na int cpfNumero
     cout << "Digite o CPF\n";
-
     for (i = 0; i < 11; i++)
     {
         //adiciona a formatação enquanto digita
@@ -32,8 +32,8 @@ void receberCpf()
         }
         cpf[i] = getche();
         // func atoi faz converção de int pra string
-        //cada vez que passa no loop ele vai completando a numerção do cpf
         cpfNumeros[i] = atoi(cpf);
+        //limpa o buffer pra evitar lixo de memória
         cpf[i] = ' ';
     }
     validadorCPF();
@@ -42,11 +42,10 @@ void receberCpf()
 
 void validadorCPF()
 {
-    system("CLS");
-    long long aux, aux1;
-    long long soma = 0;
-    // soma = ((cpfNumeros[0]*10)+(cpfNumeros[1]*9)+(cpfNumeros[2]*8)+(cpfNumeros[3]*7)+(cpfNumeros[4]*6)+(cpfNumeros[5]*5)+(cpfNumeros[6]*4)+(cpfNumeros[7]*3)+(cpfNumeros[8]*2));
-    //resolver divisão pra validação
+    system("CLS");  
+    long long int aux, aux1;
+    long long int soma = 0;
+    // soma segundo matematica do
     for (int i = 0; i < 9; i++)
     {
         soma += cpfNumeros[i] * (10 - i);
@@ -79,30 +78,41 @@ void validadorCPF()
     {
         digito2 = 11 - soma;
     }
-
+    //mostrar cpf com pontuação
     for (i = 0; i < 11; i++)
     {
-        cout << cpfNumeros[i];
-        if (i - 1 == 4)
+
+        if (i - 1 == 2)
+        {
+            cout << ".";
+        }
+        if (i - 1 == 5)
         {
             cout << ".";
         }
         if (i - 1 == 8)
         {
-            cout << ".";
-        }
-        if (i - 1 == 9)
-        {
             cout << "-";
         }
+        cout << cpfNumeros[i];
     }
     cout << "\nPrimeiro Digito: " << digito1;
     cout << "\nSegundo Digito: " << digito2;
     cout << "\n";
+
+    if (digito1 == cpfNumeros[9] && digito2 == cpfNumeros[10])
+    {
+        cout << "CPF Valido.";
+    }
+    else
+    {
+        cout << "CPF Invalido.";
+    }
 }
 
 main()
 {
-
     receberCpf();
+    validadorCPF();
+
 }
